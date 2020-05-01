50bd190369e7db1542c3a784b964762e232af68b
manish sharma <manish.sharma23@gmail.com>
2020-05-01 16:09:45 +0530

    clear file

--- SpringBoot_EurekaServer/src/main/java/com/eurekaserver/SpringBootEurekaServerApplication.java
+++ SpringBoot_EurekaServer/src/main/java/com/eurekaserver/SpringBootEurekaServerApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringBootEurekaServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		System.out.println();
 		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
 	}
 
